38cf621251d60ab0c0d791cf43c672512db89a97
Lionseun Ling <lionseun@gmail.com>
2017-04-27 00:14:57 +0800

    temp commit

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,11 @@
-def to_rna():
-    pass
+try:
+    from StringIO import StringIO
+except:
+    from io import StringIO
+
+
+def to_rna(dna):
+    # tdna = dna.strip().lower()
+    tdna = StringIO()
+    tdna.write(dna.strip().lower())
+    trna = StringIO()
