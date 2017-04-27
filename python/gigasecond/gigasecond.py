1f45dfb05e0f259487c54f7e4555f733b86671e0
Lionseun Ling <lionseun@gmail.com>
2017-04-27 19:47:50 +0800

    gigasecond.py is done

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,9 @@
-def add_gigasecond():
-    pass
+from datetime import timedelta
+
+
+def add_gigasecond(date):
+    '''
+    get a time, add increase 10^9 seconds
+    '''
+    gigatime = timedelta(seconds=1000000000)
+    return date + gigatime
