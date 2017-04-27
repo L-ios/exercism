3f5224a9ae5b5f951aa83a4a9923fcfc398405fd
Lionseun Ling <lionseun@gmail.com>
2017-04-27 14:06:43 +0800

    RNA transcription is done.

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -5,7 +5,32 @@ except:
 
 
 def to_rna(dna):
-    # tdna = dna.strip().lower()
-    tdna = StringIO()
-    tdna.write(dna.strip().lower())
-    trna = StringIO()
+    '''
+    * `G` -> `C`
+    * `C` -> `G`
+    * `T` -> `A`
+    * `A` -> `U`
+    '''
+    with StringIO() as tdna:
+        with StringIO() as trna:
+            tdna.write(dna)
+            tdna.seek(0)
+            while True:
+                ch = tdna.read(1)
+                if ch == '':
+                    break
+                if ch == 'G':
+                    trna.write('C')
+                elif ch == 'C':
+                    trna.write('G')
+                elif ch == 'T':
+                    trna.write('A')
+                elif ch == 'A':
+                    trna.write('U')
+                elif ch.isalpha():
+                    return ''
+                else:
+                    continue
+            trna.flush()
+            ret = trna.getvalue()
+            return ret
