6843ce73c8b6cedefeb71e554cfd052ff0f5a335
Lionseun Ling <lionseun@gmail.com>
2017-04-28 17:20:05 +0800

    finish word count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,34 @@
-def word_count():
-    pass
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+
+def word_count(string):
+    '''
+    count word in string, number is also word
+    '''
+    word_dict = {}
+    with StringIO() as origin_str:
+        origin_str.write(string.lower())
+        origin_str.seek(0)
+        word = ''
+        while True:
+            char = origin_str.read(1)
+            if char == '':
+                add_word_to_dict(word, word_dict)
+                break
+            if char.isnumeric() or char.isalpha():
+                word += char
+            else:
+                add_word_to_dict(word, word_dict)
+                word = ''
+    return word_dict
+
+
+def add_word_to_dict(word, word_dict):
+    if len(word):
+        if word_dict.get(word):
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
