d1451ce41a7944b079141f86da5be23722bbe617
Lionseun Ling <lionseun@gmail.com>
2017-05-17 19:58:05 +0800

    finish bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,8 @@
-def hey():
-    pass
+def hey(str):
+    if str.isupper():    # all letter is upper
+        return 'Whoa, chill out!'
+    if str.strip().endswith('?'):  # end with "?"
+        return 'Sure.'
+    if len(str.strip()) == 0:
+        return 'Fine. Be that way!'
+    return 'Whatever.'
